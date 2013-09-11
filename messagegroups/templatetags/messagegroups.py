cf87da93bb8822d6d29b1e4fd6eb2a659706f847
Danilo Bargen <gezuru@gmail.com>
2013-09-11 19:43:38 +0200

    Better handling of message priority levels

--- messagegroups/templatetags/messagegroups.py
+++ messagegroups/templatetags/messagegroups.py
@@ -1,13 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, division, absolute_import, unicode_literals
+
+from collections import defaultdict
+
 from django import template
+from django.contrib.messages.constants import DEBUG, INFO, SUCCESS, WARNING, ERROR
+
 register = template.Library()
+
+TAGS = {  # Bootstrap error constant mappings
+    DEBUG: 'info',
+    INFO: 'info',
+    SUCCESS: 'success',
+    WARNING: 'warning',
+    ERROR: 'danger',
+}
 
 
 @register.inclusion_tag('messagegroups.html')
 def render_messages(messages):
-    grouped = {}
+    grouped = defaultdict(set)
     for m in messages:
-        if (m.level, m.tags) in grouped:
-            grouped[(m.level, m.tags)].append(m.message)
-        else:
-            grouped[(m.level, m.tags)] = [m.message]
-    return {'messages': grouped}
+        tags = 'alert-{} {}'.format(TAGS[m.level], m.tags)
+        grouped[(m.level, tags)].add(m.message)
+    return {'messages': dict(grouped)}
